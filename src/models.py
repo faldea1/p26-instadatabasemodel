78bfd20d4a730f30719eed8b4e116df6676067f1
faldea1 <faldea1@uc.cl>
2023-01-11 04:13:08 +0000

    Start Insta DB Model + tables

--- src/models.py
+++ src/models.py
@@ -8,17 +8,19 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+##class: Here we define columns for each table. Notice that each column is also a normal Python instance attribute
+
+
+##tablas predeterminadas -> boilerplate:
+
 class Person(Base):
     __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+
 class Address(Base):
     __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     street_name = Column(String(250))
     street_number = Column(String(250))
@@ -26,8 +28,67 @@ class Address(Base):
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
+
+##tablas adicionales:
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+
+
+class Follow(Base):
+    __tablename__ = 'follow'
+    id = Column(Integer, primary_key=True)
+
+
+class Follower(User):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+
+
+class History(Base):
+    __tablename__ = 'history'
+    id = Column(Integer, primary_key=True)
+
+
+class Publicity(Base):
+    __tablename__ = 'publicity'
+    id = Column(Integer, primary_key=True)
+
+
+class Shop(Base):
+    __tablename__ = 'shop'
+    id = Column(Integer, primary_key=True)
+
+
+
+
+
+
+
+
+
+## Qué es lo que sigue abajo del boilerplate ¿? preguntar.
+
+
     def to_dict(self):
         return {}
+
 
 ## Draw from SQLAlchemy base
 try:
